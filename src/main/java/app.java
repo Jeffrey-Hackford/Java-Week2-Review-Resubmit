3a846af4f9d6725d19b57ec70cabc1b32dae0907
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-05-04 22:01:04 -0700

    add routes to vtl pages and more unit specs

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,17 +1,55 @@
-// import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
-//
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
-//
-//     get("/", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       model.put("template", "templates/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//   }
-// }
+import java.util.ArrayList;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/definitions", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String definition = request.queryParams("definition");
+      Definition newDefinition = new Definition(definition);
+      request.session().attribute("definition", newDefinition);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("definition", request.session().attribute("definition"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/definitions", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Definition> definitions = request.session().attribute("definitions");
+      if (definitions == null) {
+        definitions = new ArrayList<Definition>();
+        request.session().attribute("definitions", definitions);
+      }
+
+      String definition = request.queryParams("definition");
+      Definition newDefinition = new Definition(definition);
+      definitions.add(newDefinition);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+
+  }
+}
