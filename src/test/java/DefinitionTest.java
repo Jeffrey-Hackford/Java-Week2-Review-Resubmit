6d1392f37fc75344ab6a1ca6f7d802be9305f526
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-05-04 21:29:21 -0700

    add second unit spec to test getDefinition method

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -8,5 +8,10 @@ public class DefinitionTest {
     Definition myDefinition = new Definition("a test definition");
     assertEquals(true, myDefinition instanceof Definition);
   }
+  @Test
+  public void Definition_instantiatesWithDefinition_String() {
+    Definition myDefinition = new Definition("a test definition");
+    assertEquals("a test definition", myDefinition.getDefinition());
+  }
 
 }
